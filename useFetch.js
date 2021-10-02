55da90c14ca7e54585e3e7b44fff507b1337dbb5
Sergio Martinez <sergio.martinez@grupomisol.mx>
2021-10-02 13:14:29 -0500

    Primer commit

---
+++ useFetch.js
@@ -0,0 +1,43 @@
+import { useEffect, useRef, useState } from 'react';
+
+export const useFetch = (url) => {
+  const isMounted = useRef(true);
+  const [state, setstate] = useState({
+    data: null,
+    loading: true,
+    error: null,
+  });
+
+  useEffect(() => {
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
+
+  useEffect(() => {
+    setstate({ data: null, loading: true, error: null });
+
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => {
+        if (isMounted.current) {
+          setstate({
+            loading: false,
+            error: null,
+            data,
+          });
+        } else {
+          console.log('setState no se llamó');
+        }
+      })
+      .catch(() => {
+        setstate({
+          data: null,
+          loading: false,
+          error: 'No se pudo cargar la info',
+        });
+      });
+  }, [url]);
+
+  return state;
+};
